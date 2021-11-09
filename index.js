66004d7250285f5750285e8d743332ff1c09af96
Sam Winslow <sammywinslow@gmail.com>
2021-11-09 13:32:30 -0500

    init; fetch action defs

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-// <TODO: your plugin code here - you can base it on the code below, but you don't have to>
+export async function setupPlugin({ config, global }) {
+    const actionMap = config.action_map.split(',').map(entry => entry.split(':'))
+    const actions = await Promise.all(actionMap.map(([actionId]) => getActionDefinition(actionId)))
 
-// Some internal library function
-async function getRandomNumber() {
-    return 4
+    const actionDefinitions = []
+    actionMap.forEach(([, conversionName], index) => {
+        const action = actions[index]
+        actionDefinitions.push({
+            id: action.id,
+            eventDetails: action.steps[0],
+            conversionName,
+        })
+    })
+
+    console.log({ actionDefinitions })
+
 }
 
-// Plugin method that runs on plugin load
-export async function setupPlugin({ config }) {
-    console.log(`Setting up the plugin`)
+async function getActionDefinition(actionId) {
+    const response = await posthog.api.get(`/api/projects/@current/actions/${actionId}/`, {
+        host: 'http://localhost:8000',
+    })
+    if (response.status !== 200) {
+        throw new Error(`Failed to get action definition for ${actionId}: ${body.detail}`)
+    }
+    const body = await response.json()
+    if (!body.steps || body.steps.length !== 1) {
+        throw new Error(`Action ${actionId} should have no more than 1 step (found ${action.steps?.length ?? 0})`)
+    }
+    return body
 }
 
-// Plugin method that processes event
-export async function processEvent(event, { config, cache }) {
-    const counterValue = (await cache.get('greeting_counter', 0))
-    cache.set('greeting_counter', counterValue + 1)
-    if (!event.properties) event.properties = {}
-    event.properties['greeting'] = config.greeting
-    event.properties['greeting_counter'] = counterValue
-    event.properties['random_number'] = await getRandomNumber()
-    return event
-}
+// export async function exportEvents(events, { config }) {
+    // check if the event is an action we care about
+// }
+
+// async function uploadConversion(gclid) {
+
+// }
